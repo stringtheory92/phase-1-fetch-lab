5f825f6b349efb7daea3f21685ed5a463ce81cfd
Adam Kabak <adamkabak@yahoo.com>
2022-09-22 12:59:35 -0400

    commit tree d43b8e329c790746e2cea8faf22f5d86857ea64a

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(data => renderBooks(data))
   
 }
 
@@ -15,3 +18,39 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
+// fetch('https://anapioficeandfire.com/api/books')
+// .then(resp => resp.json())
+// .then(data => console.log(data));
+
+// // Gets the total pages of the series, 
+// // the total pov characters in the series,
+// // and the 60th pov character (in array)
+// fetch('https://anapioficeandfire.com/api/books')
+// .then(resp => resp.json())
+// .then(data => {
+//   console.log(data);
+//   let pagesTotal = 0;
+//   let charTotal = 0;
+//   let sixtiethCharFound = false;
+
+//   for (let book of data) {
+//     const pagesPerBook = book.numberOfPages
+//     pagesTotal += pagesPerBook
+//   }
+//   console.log('pages: ', pagesTotal)
+
+//   for (let book of data) {
+//     const charPerBook = book.povCharacters.length;
+//     charTotal += charPerBook;
+//     if (charTotal === 60) console.log(book.povCharacters.slice(-1));
+//     if (charTotal > 60 && sixtiethCharFound === false) {
+//       const sliceValue = 60 - charTotal;
+//       console.log('sliceValue: ', sliceValue, 'charTotal: ', charTotal)
+//       console.log(book.povCharacters.slice(sliceValue, sliceValue + 1))
+//       sixtiethCharFound = true
+//     }
+//   }
+//   console.log('Chars: ', charTotal);
+// });
